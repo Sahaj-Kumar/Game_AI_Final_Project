b0f017cffd658190fdc760d88851aa9648671df3
Sahaj-Kumar <sahaj.kumar@gmail.com>
2018-03-05 22:59:33 -0500

    Basic Jumping, Camera Tweaking.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,17 +4,17 @@ public class CameraFollow : MonoBehaviour {
 
 	public Transform target;
 	public Vector3 positionOffset;
-	public Vector3 rotationOffset;
 	public float smoothFactor = 0.125f;
 
 	private Vector3 velocity = Vector3.zero;
 
-	void LateUpdate () {
+	// NOTE: If target movement is NOT reliant on in-built, use LateUpdate rather than FixedUpdate.
+	void FixedUpdate () {
 
 			Vector3 desiredPosition = target.position + positionOffset;
-			Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothFactor);
+			Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothFactor * Time.deltaTime);
 			transform.position = smoothedPosition;
-			//transform.eulerAngles = rotationOffset;
 			transform.LookAt(target);
+
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	Rigidbody rb;
-	public float acceleration = 1.0f;
-	public float maxVelocity = 5.0f;
+	private Rigidbody rb;
 
-	public float walkSpeed = 1.0f;
-	public float runSpeed = 2.0f;
+	public float jumpVelocity = 10.0f;
+	public float walkSpeed = 5.0f;
+	public float runSpeed = 10.0f;
 
 	public float smoothTurn = 0.1f;
 	private float smoothTurnVelocity;
@@ -18,37 +17,77 @@ public class PlayerController : MonoBehaviour {
 	private float smoothSpeedVelocity;
 	private float currentSpeed;
 
+	private bool jumpRequest = false;
+
 	// TODO setup animator component
 
 	private KeyCode RUNNING_KEY = KeyCode.LeftShift;
-	//private KeyCode JUMP_KEY = KeyCode.Space;
+	private KeyCode JUMP_KEY = KeyCode.Space;
 
-	// Use this for initialization
-	void Start () {
+	Vector3 playerSize; 					// Player's box collision dimensions.
+	public float groundedSkin = 0.05f;		// How close player is to floor to be considered 'grounded'.
+	Vector3 boxSize;						// Box casting dimensions to check if grounded.
+	public LayerMask mask;					// Layers considered when checking groundedness (set as 'default').
+	private bool grounded = false;
+
+	void Awake () {
 		rb = gameObject.GetComponent<Rigidbody>();
-		Debug.Log("Hello world!");
+		playerSize = GetComponent<BoxCollider>().size;
+		boxSize = new Vector3(playerSize.x, groundedSkin, playerSize.z);
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
+	void Update() {
+		// Handle jump input before handling in FixedUpdate.
+		if (Input.GetKeyDown(JUMP_KEY) && grounded) {
+			jumpRequest = true;
+		}
+	}
+
+	/*
+		Handles player movement, which is partially	based off built-in physics.
+		Also, it was the only way to make the 3rd person camera stop being jittery :/
+	*/
+	void FixedUpdate () {
+		MoveHandler();
+		JumpHandler();
+	}
+
+	/**
+		Handles 'joystick' input for walking and running.
+		This is done with translation. (No physics)
+	*/
+	void MoveHandler() {
+		// Get input vector.
 		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 		Vector2 inputDirection = input.normalized;
 
+		// Set player's facing direction (with smoothing).
 		if (inputDirection != Vector2.zero) {
 			float targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.y) * Mathf.Rad2Deg;
 			transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y,targetRotation, ref smoothTurnVelocity, smoothTurn);
 		}
 
+		// Translate player given input magnitude (with smoothing).
 		bool running = Input.GetKey(RUNNING_KEY);
 		float targetSpeed = (running ? runSpeed : walkSpeed) * inputDirection.magnitude;
-
 		currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref smoothSpeedVelocity, smoothSpeed);
-
 		transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
+	}
 
-		// TODO: update animator
-
+	/*
+		Handles jumping (relies on jumpRequest status) and tracks player groundedness.
+		This is done by applying forces to the player's rigidbody.
+	*/
+	void JumpHandler() {
+		// Execute jump if permitted.
+		if (jumpRequest) {
+			Debug.Log("Jumping!");
+			rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+			jumpRequest = false;
+		}
+		// Update grounded status.
+		Vector3 boxCenter = transform.position + Vector3.down * (playerSize.y + boxSize.y) * 0.5f;
+		grounded = (Physics.OverlapBox(boxCenter, boxSize, Quaternion.identity, mask).Length > 0);
 	}
 
 }
